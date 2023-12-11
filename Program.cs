7dcf8f5763f82e78b0dbc0a4e43d60865a9baf02
olimov Amir <olimov.amir@mail.ru>
2023-12-11 10:10:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte aggression = 1;
+        byte democracyModifier = 2;
+
+        aggression = (byte)(aggression - democracyModifier); //  сужающее преобразование ланных
+
+        Console.WriteLine(aggression);
+
+
+        int a = int.MaxValue;
+        a = a + 1; // переполнение через верзную границу
+        Console.WriteLine(a);
+
+        int b = int.MinValue;
+        b = b - 1; // переполнение через нижную границу
+        Console.WriteLine(b);
+    }
+}
